69166954ab274fcbedf7a66fe387af6505a770eb
adamGiusti <adamgiusti@Adams-MacBook-Pro.local>
2020-07-02 09:26:29 -0700

    Add and initiate CoffeeMachine class file

---
+++ src/CoffeeMachine.java
@@ -0,0 +1,12 @@
+public class CoffeeMachine {
+    // Initial resources available
+    private int mlWater = 400;
+    private int mlMilk = 540;
+    private int gramsCoffeeBeans = 120;
+    private int numDisposableCups = 9;
+    private double moneyAmount = 550;
+
+    // Execution point of coffee machine
+    public static void start() {
+    }
+}
