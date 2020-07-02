aea5507ee6b7b6f5c7c3d68a1f4a15c47387d2f4
adamGiusti <adamgiusti@Adams-MacBook-Pro.local>
2020-07-02 09:36:06 -0700

    Add coffee machine's greet method

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -8,5 +8,17 @@ public class CoffeeMachine {
 
     // Execution point of coffee machine
     public static void start() {
+        greet();
+    }
+
+    // Introduce the machine to the user
+    private static void greet() {
+        System.out.println("Greetings! I'm a virtual coffee machine here to serve you.");
+        System.out.println();
+        System.out.println("I work with typical products: water, milk, coffee beans, and plastic cups; if I run out of something, I'll notify you.");
+        System.out.println("You can get three types of coffee: espresso, cappuccino, and latte.");
+        System.out.println();
+        System.out.println("Go ahead and interact with me by writing one of the available actions below:");
+        System.out.println();
     }
 }
